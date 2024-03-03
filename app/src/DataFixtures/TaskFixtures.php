9f7b553162367d32f7dbf51605d0f562a132da92
RafaelCoppe <rafael.coppe.jr@gmail.com>
2024-03-03 21:45:53 +0100

    commit 1

---
+++ app/src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TaskFixtures extends Fixture
+{
+    CONST tasks = [
+        ["label" => "Faire les courses", "done" => false],
+        ["label" => "Appeler le médecin", "done" => true],
+        ["label" => "Répondre aux emails", "done" => false],
+        ["label" => "Aller à la gym", "done" => false],
+        ["label" => "Lire un chapitre du livre", "done" => true],
+        ["label" => "Préparer le rapport", "done" => false],
+        ["label" => "Nettoyer la cuisine", "done" => true],
+        ["label" => "Réviser pour l'examen", "done" => false],
+        ["label" => "Apprendre une nouvelle recette", "done" => false],
+        ["label" => "Sortir le chien", "done" => true]
+    ];
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::tasks as $task) {
+            for ($i = 0; $i < 10; $i++) {
+                $task = new Task();
+                $task->setLabel($task['label']);
+                $task->setDone($task['done']);
+                $manager->persist($task);
+            }
+        }
+
+        $manager->flush();
+    }
+}
